155f30adf8ae5a20dfbc70c4bd954fcf29b52398
ivanius51 <ivanius51@gmail.com>
2018-05-27 22:54:45 +0300

    fix syntax errors

--- CustomRangeRadius.lua
+++ CustomRangeRadius.lua
@@ -1,43 +1,51 @@
 local CustomRangeRadius = {}
+CustomRangeRadius.Menu = {}
+CustomRangeRadius.User = {}
+CustomRangeRadius.Particles = {}
 
 CustomRangeRadius.Menu.Path = {"Utility", "Custom Range Radius"}
 CustomRangeRadius.Menu.Enabled = Menu.AddOptionBool(CustomRangeRadius.Menu.Path, "Enabled", false)
 CustomRangeRadius.Menu.Radius = Menu.AddOptionSlider(CustomRangeRadius.Menu.Path, "Radius", 150, 2500, 1300)
+
 CustomRangeRadius.User.Hero = nil
 CustomRangeRadius.Particles.Radius = {}
 
 function CustomRangeRadius.isEnabled()
-	return Menu.IsEnabled(CustomRangeRadius.Menu.Enabled);
+	return Menu.IsEnabled(CustomRangeRadius.Menu.Enabled)
 end
 
 function CustomRangeRadius.getRadius()
-	return Menu.IsEnabled(CustomRangeRadius.Menu.Radius);
+	return Menu.GetValue(CustomRangeRadius.Menu.Radius)
 end
 
 function CustomRangeRadius.Initialization()
 	CustomRangeRadius.User.Hero = nil
-	for i in pairs(CustomRangeRadius.Particles.Radius) do
-		CustomRangeRadius.Particles.Radius[i] = nil
+	for k in pairs(CustomRangeRadius.Particles.Radius) do
+		CustomRangeRadius.Particles.Radius[k] = nil
+	end
 end
 
-function CustomRangeRadius.SetRange(index,radius)
+function CustomRangeRadius.Finalization()
+	CustomRangeRadius.User.Hero = nil
+	for k in pairs(CustomRangeRadius.Particles.Radius) do
+		CustomRangeRadius.Particles.Radius[k] = nil
+	end
+end
+
+function CustomRangeRadius.SetRange(index,range)
 	if CustomRangeRadius.User.Hero == nil then
 		return false
-	end
-	if ((CustomRangeRadius.Particles.Radius[index] == nil) or (CustomRangeRadius.Particles.Radius[index].ID == 0)) then
+	elseif ((CustomRangeRadius.Particles.Radius[index] == nil) or (CustomRangeRadius.Particles.Radius[index].ID == 0)) then
 		CustomRangeRadius.Particles.Radius[index] = {}
-		CustomRangeRadius.Particles.Radius[index].ID = Particle.Create("particles/range_display_blue.vpcf", Enum.ParticleAttachment.PATTACH_ABSORIGIN_FOLLOW, CustomRangeRadius.User.Hero);
-		CustomRangeRadius.Particles.Radius[index].Value = radius;
-		Particles.SetControlPoint(CustomRangeRadius.Particles.Radius[index].ID, 1, [radius,0,0])
+		CustomRangeRadius.Particles.Radius[index].ID = Particle.Create("particles/range_display_blue.vpcf", Enum.ParticleAttachment.PATTACH_ABSORIGIN_FOLLOW, CustomRangeRadius.User.Hero)
+		CustomRangeRadius.Particles.Radius[index].Value = range
+		Particles.SetControlPoint(CustomRangeRadius.Particles.Radius[index].ID, 1, Vector(range,0,0))
 		return true
-	else
-		if CustomRangeRadius.Particles.Radius[index].Value ~= radius then
-			Particles.SetControlPoint(CustomRangeRadius.Particles.Radius[index].ID, 1, [radius,0,0])
-		else
-			return false
-		end
+	elseif CustomRangeRadius.Particles.Radius[index].Value ~= range then
+		Particles.SetControlPoint(CustomRangeRadius.Particles.Radius[index].ID, 1, Vector(range,0,0))
 		return true
 	end
+	return false
 end
 
 function CustomRangeRadius.OnUpdate(p1)
@@ -46,7 +54,7 @@ function CustomRangeRadius.OnUpdate(p1)
 		return 
 	else
 		if CustomRangeRadius.isEnabled() then
-			//main radius will be replaced with for loop
+			-- main radius will be replaced with for loop
 			CustomRangeRadius.SetRange(0,CustomRangeRadius.getRadius())
 		else
 			for i in pairs(CustomRangeRadius.Particles.Radius) do
@@ -54,9 +62,9 @@ function CustomRangeRadius.OnUpdate(p1)
 					Particle.Destroy(CustomRangeRadius.Particles.Radius[i].ID)
 					CustomRangeRadius.Particles.Radius[i].ID = 0
 				end
+			end
 		end
 	end
-	
 end
 
 function CustomRangeRadius.OnGameStart()
@@ -64,7 +72,7 @@ function CustomRangeRadius.OnGameStart()
 end
 
 function CustomRangeRadius.OnGameEnd()
-	CustomRangeRadius.Initialization()
+	CustomRangeRadius.Finalization()
 end
 
 return CustomRangeRadius
